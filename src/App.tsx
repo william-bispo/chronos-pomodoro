f3b2da306406cedd09925f4de4a77eb9227dcfaa
William Bispo <willian.bispo.matos@gmail.com>
2025-12-15 11:08:56 -0300

    Heading and screenshot
    Added lucide-react and some changes in Heading component. Also added the
    final app screenshots.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <div>
-      <Heading>Hello World!!!</Heading>
+      <Heading>
+        Hello World!!!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>Teste123</p>
     </div>
   );
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,9 @@
 import styles from './Heading.module.css';
 
-export function Heading({ children }) {
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
   return <h1 className={styles.heading}>{children}</h1>;
 }
